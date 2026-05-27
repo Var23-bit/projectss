15f653c815fc5eb635754218dd87efa313b932d6
Varun <your.email@example.com>
2026-05-27 17:52:05 +0530

    trying to make api get request

---
+++ node2.js
@@ -0,0 +1,10 @@
+function getrandomnumber(){
+    return Math.floor(Math.random()*100)+1;
+}
+function celciusToFahrenheit(celsius) {
+    return (celsius * 9/5) + 32;
+}
+
+module.exports ={getrandomnumber, celciusToFahrenheit};
+
+;
---
+++ node3.js
@@ -0,0 +1,6 @@
+/*const {getrandomnumber,celciusToFahrenheit} = require('./node2');
+console.log(`random number: ${getrandomnumber()}`);
+
+console.log(`89°C in Fahrenheit: ${celciusToFahrenheit(89)}`);*/
+import {getPosts} from './post.js';
+console.log(getPosts());
