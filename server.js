02fd6c797222ac93ceef6ca49771c910e0d9070c
Varun <varunkumar62058@gmail.com>
2026-05-28 18:45:26 +0530

    changes in get url back

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 import http from 'http';
-
+const PORT = process.env.PORT || 5000;
 const server = http.createServer((req, res) => {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World!');
+    console.log(req.url);
+    console.log(req.method);
+    res.writeHead(500, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify({ message: 'Server error' }));
 });
 
-server.listen(4000, () => {
-    console.log('Server running on port 4000');
+server.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
