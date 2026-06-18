d4dc4dbc58811c66a8c1f49da9d71b0eedc65a18
Varun <varunkumar62058@gmail.com>
2026-06-18 20:25:51 +0530

    error finiding in specific portation

--- promisechain.js
+++ promisechain.js
@@ -4,15 +4,20 @@ promise.then(function(orderID){
     console.log(orderID);
     //return processpayment(orderID);
 })
+.catch(function(error){
+    console.log(error.message);
+});
 function createorder(cart) {
    const pr = new Promise(function(resolve,reject) {
     if(!validateCart(cart)) {
-        const error = new Error("Invalid cart");
+        const error = new Error("cart is not valid");
         reject(error);
     } 
     const orderID = "1234";
     if(orderID){
-        resolve(orderID);
+        setTimeout(function() {
+
+        resolve(orderID);},5000);
     }   
  });
  return pr;
