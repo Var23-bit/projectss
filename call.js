f0a95bf84727cdb04028548ea059d79212797e23
Varun <varunkumar62058@gmail.com>
2026-06-23 20:03:17 +0530

    call function

---
+++ call.js
@@ -0,0 +1,19 @@
+"use strict"
+console.log(this);
+
+function x(){
+    console.log(this);
+}
+x();
+//window.x();
+const obj = {
+    name: "naveen",
+    x: function(){
+        console.log(this.name);
+    }
+}
+obj.x();
+const obj2 = {
+    name: "alice",
+}
+obj.x.call(obj2);
